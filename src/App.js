50a1add5db8d7094f2797b5111e73371e04ed740
Nik <radionov@me.com>
2018-11-03 19:21:19 +0300

    0.0.68 - revises history

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ import Offer from './components/Offers/Offer'
 import Users from './components/Users/Users'
 import User from './components/Users/User'
 import Conversions from './components/Conversions'
+import Revises from './components/Revises'
 
 const NoMatch = ({ location }) => (
   <div>
@@ -105,6 +106,10 @@ const routes = [
   {
     path: '/conversions',
     component: Conversions,
+  },
+  {
+    path: '/revises',
+    component: Revises,
   },
   {
     path: '/news',
@@ -293,6 +298,7 @@ class App extends Component {
                   {/* <li><NavLink to="/promo">{icons.promo} {t('menu.promo')}</NavLink></li> */}
                   <li><NavLink to="/stats">{icons.stats} {t('menu.stats')}</NavLink></li>
                   <li><NavLink to="/conversions">{icons.stats} Конверсии</NavLink></li>
+                  <li><NavLink to="/revises">{icons.stats} Сверки</NavLink></li>
                   <li><NavLink to="/users">{icons.support} Пользователи</NavLink></li>
                   <li><NavLink to="/finance">{icons.finance} {t('menu.finance')}</NavLink></li>
                   <li><NavLink to="/tickets" className="relative">{icons.support} {t('menu.support')} {unreadMessages > 0 && <span className="badge">{unreadMessages}</span>}</NavLink></li>
---
+++ src/components/Revises/index.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import { Form, Table, Icon, Divider, Select, Input, Button, message, Popconfirm, Tooltip,  DatePicker, Modal, Popover } from 'antd'
+import Helpers, { Events, Filters, t, pick, clean, disabledDate, TreeSelectRemote, queryParams } from '../../common/Helpers'
+import api from '../../common/Api'
+import axios from 'axios'
+import { connect } from 'react-redux'
+import moment from 'moment'
+import locale from 'antd/lib/date-picker/locale/ru_RU'
+import qs from 'qs'
+import Filter from './Filter'
+import _ from 'lodash'
+import * as Feather from 'react-feather';
+
+
+const Icons = {}
+
+const Option = Select.Option
+const { RangePicker } = DatePicker
+
+
+const initialFilter = {
+  //'q[lead_type][equal]': 'all',
+  //'q[created_at][between]': `${moment().subtract(14, "days").startOf('day').utcOffset(Helpers.time_offset()).unix()},${moment().endOf('day').utcOffset(Helpers.time_offset()).unix()}`,
+}
+
+class Revises extends Component {
+
+  constructor(props) {
+    super(props)
+    const tmp = Filters.parse()
+    this.state = {
+      data: [],
+      filters: (_.isEmpty(tmp) && { ...initialFilter }) || tmp,
+      types: {},
+      statuses: {},
+      pagination: {
+        hideOnSinglePage: true,
+      },
+      selectedRowKeys: [],
+      selectedActionIndexIds: [],
+      columns: [
+        {
+          title: 'ID',
+          dataIndex: 'id',
+          sorter: true,
+        }, {
+          title: 'Название',
+          dataIndex: 'title',
+          sorter: true,
+        }, {
+          title: 'Дата создания',
+          dataIndex: 'created_at',
+          sorter: true,
+          width: 150,
+          render: (text, { created_at }) => created_at && moment.unix(created_at).format('DD.MM.YY HH:mm')
+        }, {
+          title: 'Тип',
+          dataIndex: 'type',
+        }, {
+          title: 'Депов в сверке',
+          dataIndex: 'lead_count_revise'
+        }, {
+          title: 'Депов в системе',
+          dataIndex: 'lead_count_system'
+        }, {
+          title: 'Разница',
+          dataIndex: 'lead_count_diff'
+        }, {
+          title: 'Принято',
+          dataIndex: 'count_confirmed'
+        }, {
+          title: 'Принято с корректировкой',
+          dataIndex: 'count_corrected'
+        }, {
+          title: 'Холд',
+          dataIndex: 'count_hold'
+        }, {
+          title: 'Отклонено',
+          dataIndex: 'count_rejected'
+        }, {
+          title: 'Ожидает корректировки',
+          dataIndex: 'count_waiting'
+        }, {
+          title: 'Доход ПП',
+          dataIndex: 'pp_income',
+          render: (text) => `${text || 0}$`
+        }
+      ]
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetch()
+    Events.follow('leads.fetch', this.fetch)
+
+    api.get('/v1/conversions/statuses')
+    .then(response => this.setState({ statuses: response.data }))
+  }
+
+  componentWillUnmount = () => {
+    Events.unfollow('leads.fetch', this.fetch)
+  }
+
+  fetch = () => {
+    const { filters, pagination } = this.state
+    this.setState({ isLoading: true })
+
+    api.get('/v1/hit-action-revises', {
+      params: {
+        sort: pagination.sort || '-id',
+        page: pagination.current || 1,
+        ...filters,
+        'per-page': pagination.pageSize,
+        //currency_id: 1,
+        //expand: 'stream,action,city,country,currency,offer,device,platform,webmaster,user,offer.advertiser',
+      }
+    })
+    .then(response => {
+      pagination.total = parseInt(response.headers['x-pagination-total-count'])
+      this.setState({
+        isLoading: false,
+        data: response.data,
+        pagination
+      })
+      Filters.toUrl(filters)
+    })
+  }
+
+  handleTableChange = ({ current: page }, filters, { columnKey, order }) => {
+    const sort = order == 'ascend' ? columnKey : `-${columnKey}`
+    const pagination = { ...this.state.pagination, current: page, sort: sort }
+    this.setState({ pagination }, this.fetch)
+  }
+
+  onFilter = (values) => {
+    const filters = Filters.prepare(values)
+    /* const group = values.group
+    filters.group = group
+    // filters.sort = `-${group}`
+    if(group.includes('sub_id_')) {
+      filters['expand'] = group.replace(`_key`, '')
+    } else if(group == 'device_type_id') {
+       filters['expand'] = 'device'
+    } else {
+      filters['expand'] = group.replace('_id', '')
+    } */
+    this.setState({ filters }, this.fetch)
+  }
+
+  render() {
+    const { data, columns, pagination, isLoading } = this.state
+    return (
+      <div>
+        <Filter initialFilter={initialFilter} onSubmit={this.onFilter} />
+        <Table
+          ref="tbl"
+          className="stats__table stats__table-leads"
+          columns={columns}
+          rowKey={(item, i) => i}
+          dataSource={data}
+          pagination={pagination}
+          loading={isLoading}
+          locale={{ emptyText: Helpers.emptyText }}
+          onChange={this.handleTableChange} />
+      </div>
+    )
+  }
+}
+
+export default Revises
